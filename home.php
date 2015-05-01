c54c742a24d11e21c5f5233876d03bd2754a37ba
JyTy <janko@pozitivendesign.net>
2015-05-01 19:04:08 +0200

    Show featured plugins on home.php

--- home.php
+++ home.php
@@ -3,20 +3,27 @@
 <div class="container" role="main">
     <div class="row">
         <div class="col-md-9">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-                <div class="page-header">   
-                    <h1><?php the_title(); ?></h1>
-                </div>
-
-                <?php the_content();?>
-
-            <?php endwhile; else: ?>
-                <div class="page-header">   
-                    <h1>Oh no!</h1>
-                </div>
-                <p>We could not find this page!!!</p>
-            <?php endif; ?>
+             <?php
+                // Get featured plugins
+                $args = array(
+                    'post_type' => 'wplugins',
+                    'posts_per_page' => -1,
+                    'meta_query'    => array(
+                        'relation'      => 'AND',
+                        array(
+                            'key'       => 'feat',
+                            'value'     => TRUE,
+                            'compare'   => '=='
+                        )
+                    )
+                );
+                $query = new WP_Query( $args );
+            ?>
+                <!-- the loop -->
+                <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                    <div class="title"><?php the_title(); ?></div>
+                <?php endwhile; ?>
+                <!-- end of the loop -->
         </div>
     </div>
 </div> <!-- END container -->
